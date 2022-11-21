5ced615b7733e31eefb81d515116d29181af2232
unknown <aquinasmoriasi@gmail.com>
2022-11-21 22:28:36 +0300

    add controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery
- 
+
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,9 @@
+class ExpensesController < ApplicationController
+  def index
+    @expenses = Expense.all
+  end
+
+  def show
+    @expense = Expense.find(params[:id])
+  end
+end

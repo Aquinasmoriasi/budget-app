00bfdd5789741b684ae0b5a2ca4384e2da2d1c5f
unknown <aquinasmoriasi@gmail.com>
2022-11-22 00:13:07 +0300

    add views structure for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,24 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    group = Group.new(group_params)
+    group.author = current_user
+    if group.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end

26c55839d36de1dab0250af5fea1282573cccef1
unknown <aquinasmoriasi@gmail.com>
2022-11-26 02:28:49 +0300

    add model specs

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Expense model' do
+    before(:each) do
+      @user = User.create name: 'Anonymus', email: 'anonymus@anon.co', password: 'password'
+    end
+
+    it 'accepts a valid name and amount' do
+      expense = Expense.create! name: 'Pork ribs', amount: 13, author: @user
+      expect(expense).to be_valid
+    end
+  end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Group model' do
+    before(:each) do
+      @user = User.create name: 'Anonymus', email: 'anonymus@anon.co', password: 'password'
+    end
+
+    it 'accepts a valid name' do
+      group = Group.create! name: 'McGyver', icon: 'shorturl.at/nxQZ7', user: @user
+      expect(group).to be_valid
+    end
+
+    it 'accepts a valid icon url' do
+      group = Group.create! name: 'McGyver', icon: 'shorturl.at/nxQZ7', user: @user
+      expect(group).to be_valid
+    end
+  end
 end
